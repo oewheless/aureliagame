5dce56eaa777098a01abc7ab6f711a52adcd00ef
oewheless <oewheless@gmail.com>
2026-03-23 07:31:17 -0400

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AureliaGame.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
